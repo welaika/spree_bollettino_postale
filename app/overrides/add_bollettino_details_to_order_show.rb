c80348aac807a5582f2755b0dd4e4979f1b3a147
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-07-25 11:39:05 +0200

    Add original deface hash

--- app/overrides/add_bollettino_details_to_order_show.rb
+++ app/overrides/add_bollettino_details_to_order_show.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'add_bollettino_details_to_order_show',
   insert_bottom: '.payment-info',
-  partial: 'spree/orders/bollettino_postale_details'
+  partial: 'spree/orders/bollettino_postale_details',
+  original: 'afb5050a40727eb8b37f6945a46719f38c3dc82b'
 )
