fc94f558cc7ce29e1f8bbd541d0d596de777a9d4
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-09-07 11:54:37 +0200

    Update override original

--- app/overrides/add_bollettino_details_to_order_show.rb
+++ app/overrides/add_bollettino_details_to_order_show.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'add_bollettino_details_to_order_show',
   insert_bottom: '.payment-info',
-  original: 'afb5050a40727eb8b37f6945a46719f38c3dc82b',
+  original: '95ca8bc3621ffd0a4d1090959628a330950840c7',
   partial: 'spree/orders/bollettino_postale_details'
 )
