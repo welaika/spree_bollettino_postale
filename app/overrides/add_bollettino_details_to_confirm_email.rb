290f7a2ea062cbb959a3ed7ba4739f72d26134b8
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-09-07 11:14:40 +0200

    Add missing deface originals

--- app/overrides/add_bollettino_details_to_confirm_email.rb
+++ app/overrides/add_bollettino_details_to_confirm_email.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/order_mailer/confirm_email',
   name: 'add_bollettino_details_to_confirm_email',
   insert_bottom: "[data-hook='payment-method']",
+  original: '13e4b9e9f438f58b15659056e73dd61b9ff23990',
   partial: 'spree/order_mailer/bollettino_postale_details'
 )
