1f424a9534519458544eaabbb2d72cd56626e787
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-09-07 12:07:03 +0200

    Remove originals sha

--- app/overrides/add_bollettino_details_to_order_show.rb
+++ app/overrides/add_bollettino_details_to_order_show.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'add_bollettino_details_to_order_show',
   insert_bottom: '.payment-info',
-  original: '95ca8bc3621ffd0a4d1090959628a330950840c7',
   partial: 'spree/orders/bollettino_postale_details'
 )
